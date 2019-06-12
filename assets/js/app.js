b26f3d9b86d88ad97ab141cf7cadcc55dd2d6350
chriscatolico <47269593+chriscatolico@users.noreply.github.com>
2019-06-12 02:02:15 -0700

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,143 +1,142 @@
 // @TODO: YOUR CODE HERE!
 // Store width and height parameters to be used in later in the canvas
-var svgWidth = 1000;
-var svgHeight = 700;
 
-// Set svg margins 
-var margin = {
-  top: 40,
-  right: 40,
-  bottom: 80,
-  left: 90
-};
+function makeResponsive() {
 
-// Create the width and height based svg margins and parameters to fit chart group within the canvas
-var width = svgWidth - margin.left - margin.right;
-var height = svgHeight - margin.top - margin.bottom;
+    // if the SVG area isn't empty when the browser loads,
+    // remove it and replace it with a resized version of the chart
+    var svgArea = d3.select("body").select("svg");
 
-// Create the canvas to append the SVG group that contains the states data
-// Give the canvas width and height calling the variables predifined.
-var svg = d3.select("#scatter")
-  .append("svg")
-  .attr("width", svgWidth)
-  .attr("height", svgHeight);
+    // clear svg is not empty
+    if (!svgArea.empty()) {
+    svgArea.remove();
+    }
 
-// Create the chartGroup that will contain the data
-// Use transform attribute to fit it within the canvas
-var chartGroup = svg.append("g")
-  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+    // SVG wrapper dimensions are determined by the current width and
+    // height of the browser window.
+    var svgWidth = window.innerWidth;
+    var svgHeight = window.innerHeight;    
 
-// Import Data
-var file = "assets/data/data.csv"
 
-// Function is called and passes csv data
-d3.csv(file).then(successHandle, errorHandle);
+    var svgWidth = window.innerWidth;
+    var svgHeight = window.innerHeight;
 
-// Use error handling function to append data and SVG objects
-// If error exist it will be only visible in console
-function errorHandle(error) {
-  throw err;
+    // Set svg margins 
+  var margin = {
+    top: 200,
+    bottom: 200,
+    right: 200,
+    left: 200
+  };
+
+
+    // Create the width and height based svg margins and parameters to fit chart group within the canvas
+    var width = svgWidth - margin.left - margin.right;
+    var height = svgHeight - margin.top - margin.bottom;
+
+    // Create the canvas to append the SVG group that contains the states data
+    // Give the canvas width and height calling the variables predifined.
+    var svg = d3.select("#scatter")
+      .append("svg")
+      .attr("width", svgWidth)
+      .attr("height", svgHeight);
+
+    // Create the chartGroup that will contain the data
+    // Use transform attribute to fit it within the canvas
+    var chartGroup = svg.append("g")
+      .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+    // Read CSV
+    d3.csv("assets/data/data.csv")
+      .then(function(stateData) {
+
+      // Parse data
+      stateData.forEach(function (data) {
+        data.poverty = +data.poverty;
+        data.obesity = +data.obesity;
+        data.income = +data.income;
+      });
+
+      console.log(stateData);
+
+      //  Create scales
+      var xLinearScale = d3.scaleLinear()
+        .domain([20, d3.max(stateData, d => d.obesity) + 2])
+        .range([0, width]);
+
+      var yLinearScale = d3.scaleLinear()
+        .domain([6, d3.max(stateData, d => d.poverty) + 2])
+        .range([height, 0]);
+
+      // Create axes
+      var xAxis = d3.axisBottom(xLinearScale);
+      var yAxis = d3.axisLeft(yLinearScale);
+
+      // Append axes
+      chartGroup.append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(xAxis);
+      chartGroup.append("g")
+        .call(yAxis);
+
+      // Create axes labels
+      chartGroup.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", margin.left - 250)
+        .attr("x", 0 - (height / 1.5))
+        .attr("dy", "1em")
+        .attr("class", "axisText")
+        .text("% of Population in Poverty");
+
+      chartGroup.append("text")
+        .attr("transform", `translate(${width / 2.5}, ${height + margin.top - 150})`)
+        .attr("class", "axisText")
+        .text("% of Population with Obesity");
+
+      // Create Circles for scatter plot
+      var circlesGroup = chartGroup.selectAll("circle")
+        .data(stateData)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xLinearScale(d.obesity))
+        .attr("cy", d => yLinearScale(d.poverty))
+        .attr("r", d => d.income/2500)
+        .attr("fill", "#1BC416")
+        .attr("opacity", ".7")
+
+      // Append text to circles 
+      var circlesGroup = chartGroup.selectAll()
+        .data(stateData)
+        .enter()
+        .append("text")
+        .attr("x", d => xLinearScale(d.obesity))
+        .attr("y", d => yLinearScale(d.poverty))
+        .style("font-size", "12px")
+        .style("text-anchor", "middle")
+        .style('fill', '#000000')
+        .text(d => (d.abbr));
+
+      // Tool tip
+      var toolTip = d3.tip()
+        .attr("class", "tooltip")
+        .offset([80, -60])
+        .html(function (d) {
+          return (`<strong>${d.state}<br>Avg Income: $${d.income}<br>Poverty: ${d.poverty}%<br>Obesity: ${d.obesity}% `);
+        });
+
+      chartGroup.call(toolTip);
+
+      circlesGroup.on("mouseover", function (data) {
+        toolTip.show(data, this);
+      })
+        .on("mouseout", function (data) {
+          toolTip.hide(data);
+        });
+    })
+
 }
+// When the browser loads, makeResponsive() is called.
+makeResponsive();
 
-// Function takes in argument statesData
-function successHandle(statesData) {
-
-  // Loop through the data and pass argument data
-  statesData.map(function (data) {
-    data.poverty = +data.poverty;
-    data.obesity = +data.obesity;
-  });
-
-  //  Create scale functions
-  // Linear Scale takes the min to be displayed in axis, and the max of the data
-  var xLinearScale = d3.scaleLinear()
-    .domain([20, d3.max(statesData, d => d.obesity) + 2])
-    .range([0, width]);
-
-  var yLinearScale = d3.scaleLinear()
-    .domain([6, d3.max(statesData, d => d.poverty) + 2])
-    .range([height, 0]);
-
-  // Create axis functions by calling the scale functions
-
-  var bottomAxis = d3.axisBottom(xLinearScale)
-    // Adjust the number of ticks for the bottom axis  
-    .ticks(7);
-  var leftAxis = d3.axisLeft(yLinearScale);
-
-
-
-
-  // Append the axes to the chart group 
-  // Bottom axis moves using height 
-  chartGroup.append("g")
-    .attr("transform", `translate(0, ${height})`)
-    .call(bottomAxis);
-  // Left axis is already at 0,0
-  // Only append the left axis 
-  chartGroup.append("g")
-    .call(leftAxis);
-
-
-  // Create Circles for scatter plot
-  var circlesGroup = chartGroup.selectAll("circle")
-    .data(statesData)
-    .enter()
-    .append("circle")
-    .attr("cx", d => xLinearScale(d.obesity))
-    .attr("cy", d => yLinearScale(d.poverty))
-    .attr("r", "13")
-    .attr("fill", "#788dc2")
-    .attr("opacity", ".75")
-
-
-  // Append text to circles 
-
-  var circlesGroup = chartGroup.selectAll()
-    .data(statesData)
-    .enter()
-    .append("text")
-    .attr("x", d => xLinearScale(d.obesity))
-    .attr("y", d => yLinearScale(d.poverty))
-    .style("font-size", "13px")
-    .style("text-anchor", "middle")
-    .style('fill', 'white')
-    .text(d => (d.abbr));
-
-  // Step 6: Initialize tool tip
-  // ==============================
-  var toolTip = d3.tip()
-    .attr("class", "tooltip")
-    .offset([80, -60])
-    .html(function (d) {
-      return (`${d.state}<br>Poverty: ${d.poverty}%<br>Obesity: ${d.obesity}% `);
-    });
-
-  // Step 7: Create tooltip in the chart
-  // ==============================
-  chartGroup.call(toolTip);
-
-  // Step 8: Create event listeners to display and hide the tooltip
-  // ==============================
-  circlesGroup.on("mouseover", function (data) {
-    toolTip.show(data, this);
-  })
-    // onmouseout event
-    .on("mouseout", function (data) {
-      toolTip.hide(data);
-    });
-
-  // Create axes labels
-  chartGroup.append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 0 - margin.left + 40)
-    .attr("x", 0 - (height / 1.5))
-    .attr("dy", "1em")
-    .attr("class", "axisText")
-    .text("% of Population in Poverty");
-
-  chartGroup.append("text")
-    .attr("transform", `translate(${width / 3}, ${height + margin.top + 10})`)
-    .attr("class", "axisText")
-    .text("% of Population with Obesity");
-}
+// When the browser window is resized, makeResponsive() is called.
+d3.select(window).on("resize", makeResponsive);
